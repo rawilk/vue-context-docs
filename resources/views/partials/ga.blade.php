4b1f4e40b101583fd1559b63add2763241661509
Randall Wilk <wilk.randall@gmail.com>
2020-05-27 13:38:14 -0500

    fix typo

--- resources/views/partials/ga.blade.php
+++ resources/views/partials/ga.blade.php
@@ -1,6 +1,6 @@
 @if (! config('app.debug'))
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('docs.googleAnalyticsId') }}"></script>
+    <script async src="https://www.googletagmanager.com/gtag.js?id={{ config('docs.googleAnalyticsId') }}"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
